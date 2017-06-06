ba5f4bcf63e0af00978619e535fb1cad011e6271
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-05 23:36:52 -0400

    renaming files

---
+++ only-tests-spec.js
@@ -0,0 +1,13 @@
+const empty = () => {}
+
+// this spec file only has individual tests
+// no describe suites
+it('d1-1', empty)
+it('d1-2', empty)
+it('d1-3', empty)
+it('d1-4', empty)
+it('d1-5', empty)
+it('d1-6', empty)
+it('d1-7', empty)
+it('d1-8', empty)
+it('d1-9', empty)
