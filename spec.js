fb2fa1e7c278a4e8a61d3cc4c825716b311f36ba
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-05 23:29:59 -0400

    initial code

---
+++ spec.js
@@ -0,0 +1,13 @@
+const empty = () => {}
+
+describe('d1', () => {
+  it('d1-1', empty)
+  it('d1-2', empty)
+  it('d1-3', empty)
+  it('d1-4', empty)
+  it('d1-5', empty)
+  it('d1-6', empty)
+  it('d1-7', empty)
+  it('d1-8', empty)
+  it('d1-9', empty)
+})
